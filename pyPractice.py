be33960a386fbce5f7011969eaa4ef2ef0260f9b
PSebs <sebabenitofrijolito12@gmail.com>
2018-10-31 15:15:14 -0500

    Add files via upload

---
+++ pyPractice.py
@@ -0,0 +1,24 @@
+#Single line comment
+'''
+Multi Line comment
+'''
+
+# Python => dynamically typed
+ 
+#int x = 5;
+x = "Name"
+y = 'a'
+z = 5
+
+List = []
+
+List.append(5)
+List.append(6)
+List.append(x)
+List.append(["a",2,3,14,'c'])
+for eachElement in List:
+	print(eachElement)
+
+print(List)
+
+
